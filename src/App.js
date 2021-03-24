6ed0edb3c22983d203b7f3fb8fe5ed03f6e8e249
akb.tech17 <akb.tech17@gmail.com>
2021-03-24 14:18:03 +0530

    communicating the items as props in Accordion from App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
+import Accordion from "./components/Accordion";
 
-const App =() => {
-    return (
-        <div>
-            Widgets App
-        </div>
-    )
-}
+const items = [
+  {
+    title: "What is React?",
+    content: "React is front-end JS Framework.",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favourite JS Library among engineers.",
+  },
+  {
+    title: "How do you use React?",
+    content: "We use React by creating Components.",
+  },
+];
+
+const App = () => {
+  return (
+    <div>
+      <Accordion items={items}/>
+    </div>
+  );
+};
 
 export default App;
-
---
+++ src/components/Accordion.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Accordion = ({items}) => {
+    return (
+        <div>
+            Accordion
+            {items.length}
+        </div>
+    )
+}
+
+export default Accordion
