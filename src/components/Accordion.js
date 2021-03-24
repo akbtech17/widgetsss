d5a1e88ab00be3731fdcad435295171ac9b98bf5
akb.tech17 <akb.tech17@gmail.com>
2021-03-24 14:30:03 +0530

    building and styling accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Accordion = ({items}) => {
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
     return (
-        <div>
-            Accordion
-            {items.length}
+      <React.Fragment key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
         </div>
-    )
-}
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
 
-export default Accordion
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+    </div>
+  );
+};
+
+export default Accordion;
