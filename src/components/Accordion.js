de500fdbbb10fca8e205d2d7b82362fea354ec0d
akb.tech17 <akb.tech17@gmail.com>
2021-03-24 15:11:54 +0530

    helper function in Functional Comonent

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-    
+    //helper function
+    const onTitleCLick = (index)=>{
+        console.log('Title clicked '+index);
+    }
+
   const renderedItems = items.map((item,index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={()=>{console.log('title clicked '+ index)}}>
+        <div className="title active" onClick={()=>{onTitleCLick(index)}}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
