70107c54103a3e46aff5d2ef5272fee4264a6e9b
akb.tech17 <akb.tech17@gmail.com>
2021-03-25 16:37:52 +0530

    created translate comp and wired-up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -40,7 +41,7 @@ const App = () => {
 
   return (
     <div>
-      
+      <Translate />
     </div>
   );
 };
