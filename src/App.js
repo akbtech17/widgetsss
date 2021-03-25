99c2e1601eb26e3dc70554f2d63ba4717b483257
akb.tech17 <akb.tech17@gmail.com>
2021-03-25 16:48:32 +0530

    cleanup App again!

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import React, { useState } from "react";
-import Accordion from "./components/Accordion";
-import Dropdown from "./components/Dropdown";
-import Search from "./components/Search";
+import React from "react";
+// import Accordion from "./components/Accordion";
+// import Dropdown from "./components/Dropdown";
+// import Search from "./components/Search";
 import Translate from "./components/Translate";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is front-end JS Framework.",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favourite JS Library among engineers.",
-  },
-  {
-    title: "How do you use React?",
-    content: "We use React by creating Components.",
-  },
-];
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is front-end JS Framework.",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favourite JS Library among engineers.",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "We use React by creating Components.",
+//   },
+// ];
 
-const options = [
-  {
-    label: "The Red Color",
-    value: "red",
-  },
-  {
-    label: "The Blue Color",
-    value: "blue",
-  },
-  {
-    label: "The Green Color",
-    value: "green",
-  },
-];
+// const options = [
+//   {
+//     label: "The Red Color",
+//     value: "red",
+//   },
+//   {
+//     label: "The Blue Color",
+//     value: "blue",
+//   },
+//   {
+//     label: "The Green Color",
+//     value: "green",
+//   },
+// ];
 
 const App = () => {
-  
   return (
     <div>
       <Translate />
