37da78aeb498bfc23afdc7e408e629c73106d1a8
akb.tech17 <akb.tech17@gmail.com>
2021-03-25 15:38:04 +0530

    setting up manual event listner on body by defining useEffect()

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+
+  //we need to set up a manual Event Listner on body element
+  //for that we need to define uE, which will run on inital render only,
+  //and set up EL
+  useEffect(() => {
+    //setting manual EL
+    document.body.addEventListener(
+      "click",
+      () => {
+        //   console.log("CLICK");
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
 
   //rendering list of options
   const renderedOptions = options.map((option) => {
@@ -25,11 +40,13 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <label className="label">Select a Color!</label>
         <div
           onClick={() => setOpen(!open)}
-          className={`ui selection dropdown ${open?'visible active':''}`}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
         >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className={`menu ${open?'visible transition':''}`}>{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
